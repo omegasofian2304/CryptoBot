ba3cb8b673c7139d64d498ed8d57184dc0a3e30f
omegasofian2304 <sofian.hussein@eduvaud.ch>
2026-08-11 17:26:49 +0200

    fix: replace localhost with 127.0.0.1 to avoid slow DNS/IPv6 resolution in redis client

---
+++ backend/services/redis_subscribe.py
@@ -0,0 +1,24 @@
+"""
+Author : Sofian Hussein
+Date : 11.08.2026
+Project : CryptoBot
+Desc : Subscribe to the c++
+"""
+import redis
+import json
+
+redis_conn = redis.Redis(
+    host='127.0.0.1',
+    port=6379,
+    decode_responses=True)
+
+def subscribe_setup(symbol):
+    pubsub = redis_conn.pubsub()
+    pubsub.subscribe(f"trend:{symbol}")
+    return pubsub
+
+
+def wait_for_trend(pubsub):
+    for message in pubsub.listen():
+        if message["type"] == "message":
+            return json.loads(message["data"])
